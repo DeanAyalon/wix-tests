081948643c9e7d2afed8d2ae4068fccd9a6c84b9
Dean Ayalon <dev@deanayalon.com>
2024-07-21 00:44:27 +0300

    Images demo - Add show/hide image elements for comparison

--- src/pages/Images.dwfbu.js
+++ src/pages/Images.dwfbu.js
@@ -1,14 +1,14 @@
 $w.onReady(() => {
-    console.log('ready');
-    $w('#repeater').onItemReady(($item, data) => {
-        console.log('itemready', data.title);
-        $item('#img').onMouseIn(() => {
-            console.log('mousein', data.title);
-            $item('#img').src = data.altImage
-        })
-        $item('#img').onMouseOut(() => {
-            console.log('mouseout', data.title);
-            $item('#img').src = data.mainImage
-        })
-    })
+    $w('#repeater').onItemReady(repeater_onItemReady)
 })
+
+// This function runs for each item in the repeater, when it's ready
+function repeater_onItemReady($item, data) {
+    // Set the mouse in/out triggers to alternate the source
+    $item('#img').onMouseIn(() => $item('#img').src = data.altImage)
+    $item('#img').onMouseOut(() => $item('#img').src = data.mainImage)
+
+    // Set the mount in/out triggers to hide/show the alternate image elements
+    $item('#img-main').onMouseIn(() => { $item('#img-main').hide(); $item('#img-alt').show() })
+    $item('#img-alt').onMouseOut(() => { $item('#img-alt').hide(); $item('#img-main').show() })
+}
