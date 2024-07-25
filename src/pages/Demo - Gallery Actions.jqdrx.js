ef391d6a74e4f1b20970ca6853144368388e6ba6
Dean Ayalon <dev@deanayalon.com>
2024-07-25 13:18:27 +0300

    Implement demo - Gallery Actions

---
+++ src/pages/Demo - Gallery Actions.jqdrx.js
@@ -0,0 +1,17 @@
+import wixData from 'wix-data'
+import wixLocation from 'wix-location'
+import wixWindow from 'wix-window-frontend'
+
+$w.onReady(() => {
+    // @ts-ignore - @wix/cli sets $w.Gallery as $w.IFrame
+    $w('#gallery').onItemClicked(async e => {
+        const img = await wixData.query('Images').eq('title', e.item.title).find().then(res => {
+            if (!res.length) throw 'Image not found in database!'
+            return res.items[0]
+        })
+
+        console.log(img)
+        if (img.redirect) wixLocation.to(img.link)
+        else wixWindow.openLightbox('Gallery Image', img)
+    })
+})
--- src/pages/Gallery Image.mzgy4.js
+++ src/pages/Gallery Image.mzgy4.js
@@ -1,10 +1,4 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+import wixWindow from 'wix-window-frontend'
 
-$w.onReady(function () {
-    // Write your JavaScript here
-
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
-});
+const img = wixWindow.lightbox.getContext()
+$w('#image').src = img.mainImage
