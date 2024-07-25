250863b650ceab63b5847b5cfff3646134e28ba5
Dean Ayalon <dev@deanayalon.com>
2024-07-25 12:10:27 +0300

    Change Demo/Pages to Demo - Pages, as @wix/cli jsconfig is generated wrong

--- src/pages/Sandbox.xque7.js
+++ src/pages/Sandbox.xque7.js
@@ -1,3 +1,6 @@
+import wixWindow from 'wix-window-frontend'
+import wixData from 'wix-data'
+
 $w.onReady(() => {
     // https://forum.wixstudio.com/t/contact-form-field/61098
     $w('#input').onCustomValidation((value, reject) => {
