91f49d8457aafbde27494689281db07c4e0722c0
Dean Ayalon <dev@deanayalon.com>
2024-07-26 08:44:56 +0300

    Sandbox - Add #spoiler, #spoilerText

--- src/pages/Sandbox.xque7.js
+++ src/pages/Sandbox.xque7.js
@@ -6,4 +6,8 @@ $w.onReady(() => {
     $w('#input').onCustomValidation((value, reject) => {
         if (!value.toLocaleLowerCase().includes('london')) reject('The word London is missing!')
     })
+
+    // https://www.reddit.com/r/WixHelp/comments/1ec4f0j/dropdown_text_boxes/
+    const spoilerText = $w('#spoilerText')
+    $w('#spoiler').onClick(() => spoilerText.collapsed ? spoilerText.expand() : spoilerText.collapse())
 })
