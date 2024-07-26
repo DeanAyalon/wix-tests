ed166e7e00edc94ebe4804cdbacef76194d53399
Dean Ayalon <dev@deanayalon.com>
2024-07-26 12:18:36 +0300

    Sandbox - Disable Store/Products.collections queryReference

--- src/pages/Sandbox.xque7.js
+++ src/pages/Sandbox.xque7.js
@@ -1,12 +1,12 @@
 import wixWindow from 'wix-window-frontend'
 import wixData from 'wix-data'
 
-// https://discord.com/channels/773211030314418196/786292021668741183/1266315235900260404
-wixData.query('Stores/Products').find().then(res => {
-    res.items.map(product => {
-        wixData.queryReferenced('Stores/Products', product, 'collections').then(res => console.log(product, res))
-    })
-})
+// // https://discord.com/channels/773211030314418196/786292021668741183/1266315235900260404
+// wixData.query('Stores/Products').find().then(res => {
+//     res.items.map(product => {
+//         wixData.queryReferenced('Stores/Products', product, 'collections').then(res => console.log(product, res))
+//     })
+// })
 
 $w.onReady(() => {
     // https://forum.wixstudio.com/t/contact-form-field/61098
