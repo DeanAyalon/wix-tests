37cd96f8fe60197bda8b30fa66cec7db228566a0
Dean Ayalon <deanayalon@me.com>
2024-10-06 18:16:11 +0300

    Add script to preview and open in browser

---
+++ preview.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Context
+cd "$(dirname "$0")"
+
+
+# Preview source
+SRC=local
+[ "$1" = "-r" ] && SRC=remote
+
+# Get URL from preview
+URL=$(wix preview --source $SRC | grep -o 'https://wix.to/[^ ]*')
+
+# Open URL in browser
+echo $URL | xargs open
