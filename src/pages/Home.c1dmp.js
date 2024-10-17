f9d767ff2a4c2b52788caf5012dcbbdfd1c2788f
Dean Ayalon <deanayalon@me.com>
2024-10-18 02:21:49 +0300

    Change homepage log message to test publish action

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -1,1 +1,4 @@
-console.log('This will only be printed if GitHub Actions successfully publishes the site')
+console.log(`Welcome to my demo website!
+    This is used for tests and demonstrations, usually when helping people in the forums
+    This site is published automatically on commit using GitHub Actions
+    In fact, this log message is being written for the sole purpose of testing it :)`)
