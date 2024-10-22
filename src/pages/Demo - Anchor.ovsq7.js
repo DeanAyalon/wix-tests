e248e0f73fdab9a47db43288c35967e61a904499
Dean Ayalon <dev@deanayalon.com>
2024-10-22 18:55:59 +0300

    Test succeeded - format code

--- src/pages/Demo - Anchor.ovsq7.js
+++ src/pages/Demo - Anchor.ovsq7.js
@@ -1,8 +1,5 @@
 import wixLocation from 'wix-location-frontend'
 
 const anchor = wixLocation.query['anchor']
-
-// $w.onReady(() => {
-    // @ts-ignore
-    if (anchor) $w('#' + anchor).scrollTo()
-// })
+// @ts-ignore
+if (anchor) $w('#' + anchor).scrollTo()
