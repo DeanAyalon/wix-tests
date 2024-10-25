0c2b0107af54b163760dddb603f09aaa60c696a5
Dean Ayalon <deanayalon@me.com>
2024-10-25 22:21:53 +0300

    Show timestamp on homepage

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -1,4 +1,10 @@
+import { timestamp, commit } from "public/release"
+import wixLocation from 'wix-location-frontend'
+
 console.log(`Welcome to my demo website!
     This is used for tests and demonstrations, usually when helping people in the forums
     This site is published automatically on commit using GitHub Actions
-    In fact, this log message is being written for the sole purpose of testing it :)`)
+    In fact, this log message was written for the sole purpose of testing it :)`)
+
+$w('#timestamp').text = `Timestamp: ${timestamp}\nCommit: ${commit}`
+$w('#timestamp').onClick(() => wixLocation.to('https://github.com/deanayalon/wix-tests/tree/' + commit))
