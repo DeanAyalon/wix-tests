cda2d321b62ca601a3866256d169a39f4417237a
Dean Ayalon <deanayalon@me.com>
2025-04-14 11:26:29 +0300

    Demo - Alternate Image comments

--- src/pages/Demo - Alternate Image.dwfbu.js
+++ src/pages/Demo - Alternate Image.dwfbu.js
@@ -4,11 +4,11 @@ $w.onReady(() => {
 
 // This function runs for each item in the repeater, when it's ready
 function repeater_onItemReady($item, data) {
-    // Set the mouse in/out triggers to alternate the source
+    // Set the mouse in/out triggers to alternate the source - loads the source each time, takes longer
     $item('#img').onMouseIn(() => $item('#img').src = data.altImage)
     $item('#img').onMouseOut(() => $item('#img').src = data.mainImage)
 
-    // Set the mount in/out triggers to hide/show the alternate image elements
+    // Set the mount in/out triggers to hide/show the alternate image elements - the pictures are both loaded, instant
     $item('#img-main').onMouseIn(() => { $item('#img-main').hide(); $item('#img-alt').show() })
     $item('#img-alt').onMouseOut(() => { $item('#img-alt').hide(); $item('#img-main').show() })
 }
